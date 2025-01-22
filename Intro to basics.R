4ab5aff358985fb649ecb6e62b218dcb159dafeb
John Westbrook <156149716+chantikoiii@users.noreply.github.com>
2025-01-22 11:18:06 -0500

    Add files via upload

---
+++ Intro to basics.R
@@ -0,0 +1,14 @@
+setwd("D:/USF SPRING 2025/LIS4370/Assign2")
+
+
+# Module 2 #
+
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+
+myMean <- function(assignment2){
+  return(sum(assignment2)/length(assignment2))
+}
+
+myMean(assignment2)
+
+mean(assignment2)
