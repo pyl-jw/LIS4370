f8c2bd5ad4297a557f36e46c880e886c8dfa018d
John Westbrook <156149716+chantikoiii@users.noreply.github.com>
2025-02-03 02:05:00 -0500

    Add files via upload

---
+++ dataframe.R
@@ -0,0 +1,6 @@
+Candidate <- c('Jeb','Donald','Ted','Marco','Carly','Hillary','Berine')
+
+ABC.Results <- c(4, 62, 51, 21, 2, 14, 15)
+CBS.Results <- c(12, 75, 43, 19, 1, 21, 19)
+
+ElectionResults16.df <- data.frame(Candidate, ABC.Results, CBS.Results)
